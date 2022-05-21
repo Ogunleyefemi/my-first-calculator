a6e2c8a0e709264c5ea48c9b91c251c6629e94ad
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-05-21 14:02:22 +0100

    add modulus

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 
-let number1 = parseInt(prompt("type any number"));
-let number2 = parseInt(prompt("type anyother number"));
-function add(number1, number2){
-    return(number1 + number2);
-}
-document.write( add(number1,number2 ));
-alert(add(number1,number2));
+// let number1 = parseInt(prompt("type any number"));
+// let number2 = parseInt(prompt("type anyother number"));
+// function add(number1, number2){
+//     return(number1 + number2);
+// }
+// document.write( add(number1,number2 ));
+// alert(add(number1,number2));
 
-subtraction
-let number1 = prompt( "put any number");
-let number2 = prompt( "any number");
-function subtract(number1, number2){
-    return(number1 - number2);
-}
-document.write( subtract(number1,number2 ));
+// subtraction
+// let number1 = prompt( "put any number");
+// let number2 = prompt( "any number");
+// function subtract(number1, number2){
+//     return(number1 - number2);
+// }
+// document.write( subtract(number1,number2 ));
 
-multiplication
-let number1 = prompt( "any number");
-let number2 = prompt( "any other number");
-function multiply(number1, number2){
-    return(number1 * number2);
-}
-document.write( multiply(number1,number2));
+// multiplication
+// let number1 = prompt( "any number");
+// let number2 = prompt( "any other number");
+// function multiply(number1, number2){
+//     return(number1 * number2);
+// }
+// document.write( multiply(number1,number2));
 
-//division
-let number1 = prompt( "any number");
-let number2 = prompt( "any number");
-function divide(number1, number2){
-    return number1 / number2
-}
-document.write( divide(number1, number2));
+// //division
+// let number1 = prompt( "any number");
+// let number2 = prompt( "any number");
+// function divide(number1, number2){
+//     return number1 / number2
+// }
+// document.write( divide(number1, number2));
 
+let number1 = parseInt(prompt( "any number"));
+let number2 = parseInt(prompt( "any number"));
+function modulus(number1, number2){
+    return number1 % number2;
+}
+document.write( modulus(number1, number2));
 
 
 
